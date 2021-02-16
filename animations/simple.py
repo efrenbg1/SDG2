55fee8bc95e14df2db11650199595129d1588d11
efrenbg1 <efren@boyarizo.es>
2021-02-16 20:32:52 +0000

    Añado animación sencilla con Python

---
+++ animations/simple.py
@@ -0,0 +1,40 @@
+import time
+import os
+
+p = [[0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0]]
+
+i = 0
+j = 0
+
+ii = 0
+jj = 0
+
+
+def paint():
+    global i, j, ii, jj
+    i += 1
+    if i > 6:
+        i = 0
+        j += 1
+    if j > 1:
+        ii += 1
+        if ii > 6:
+            ii = 0
+            jj += 1
+        p[jj][ii] = 0
+    p[j][i] = 1
+    os.system('cls')
+    for row in p:
+        print(row)
+
+
+for _ in range(1, 7*8):
+    paint()
+    time.sleep(0.1)
