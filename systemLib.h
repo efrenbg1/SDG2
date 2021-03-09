30292d62d3d93bfc502add58c806a44cc0b43174
efrenbg1 <efren@boyarizo.es>
2021-03-09 17:48:20 +0000

    Actualizado debounce time

--- systemLib.h
+++ systemLib.h
@@ -22,7 +22,7 @@
 
 // ATENCION: Valores a modificar por el alumno
 // INTERVALO DE GUARDA ANTI-REBOTES
-#define DEBOUNCE_TIME 0
+#define DEBOUNCE_TIME 250
 #define TIMEOUT_ACTUALIZA_JUEGO 0
 
 // A 'key' which we can lock and unlock - values are 0 through 3
